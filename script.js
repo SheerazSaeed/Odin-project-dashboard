e9796d0a6e16740d1d3d3ad2af824df0fc2ab968
Sheeraz Saeed <142117995+SheerazSaeed@users.noreply.github.com>
2024-04-25 19:41:39 +0100

    added button elements to html, updated CSS design for header, button onclick for main leads to recipes project

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let button = document.querySelector('.js-recipesButton');
+    button.addEventListener('click', function() {
+        window.location.href = 'https://htmlpreview.github.io/?https://github.com/SheerazSaeed/1-Odin_project_recipes/blob/main/index.html';
+    });
+});
