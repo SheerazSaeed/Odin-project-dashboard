ededbbd43fb4a68f42769660a2bd97085ff3640b
Sheeraz Saeed <142117995+SheerazSaeed@users.noreply.github.com>
2024-04-28 17:28:16 +0100

    added images to buttons on sidebar

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let button = document.querySelector('.js-recipesButton');
-    button.addEventListener('click', function() {
+    const button1 = document.querySelector('.js-recipesButton');
+    button1.addEventListener('click', function() {
         window.location.href = 'https://htmlpreview.github.io/?https://github.com/SheerazSaeed/1-Odin_project_recipes/blob/main/index.html';
+    });
+    const button2 = document.querySelector('.js-AmazonSC-button');
+    button2.addEventListener('click', function() {
+        window.location.href = 'https://github.com/SheerazSaeed/1-SS-Amazon-project';
     });
 });
