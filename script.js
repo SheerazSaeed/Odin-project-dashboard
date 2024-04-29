c0b7757ef6426fec13ab362f8d8ca9d58feaa5f6
Sheeraz Saeed <142117995+SheerazSaeed@users.noreply.github.com>
2024-04-29 14:03:04 +0100

    added background images for the first 4 project containers. Padding and margins for sidebar buttons

--- script.js
+++ script.js
@@ -10,4 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
     setupButton('.js-AmazonSC', 'https://github.com/SheerazSaeed/1-SS-Amazon-project');
     setupButton('.js-restaurantSC', 'https://github.com/SheerazSaeed/Odin-project-restaurant');
     setupButton('.js-restaurantPreview', 'https://htmlpreview.github.io/?https://github.com/SheerazSaeed/Odin-project-restaurant/blob/main/dist/index.html');
+    setupButton('.js-libraryPreview', 'https://htmlpreview.github.io/?https://github.com/SheerazSaeed/8_Odin-project-library/blob/main/Index.html');
+    setupButton('js-librarySC','https://github.com/SheerazSaeed/8_Odin-project-library');
 });
